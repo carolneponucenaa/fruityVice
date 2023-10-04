4df52f30d9c186aede25f013bd8be4b952ccdb60
Carolina Neponucena <cneponucena@gmail.com>
2023-10-04 17:27:32 -0300

    foi

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
-'use strict'
-const frutas = 'https://www.fruityvice.com/#3';
-const criarCards = (frutas) => {
+// 'use strict'
+// const frutas = 'https://www.fruityvice.com/#3';
+// const criarCards = (frutas) => {
 
-    const card = document.createElement('div')
-    card.classList.add('card')
+//     const card = document.createElement('div')
+//     card.classList.add('card')
 
-    const imagem = document.createElement('img')
-    imagem.src = `./img/${frutas.imagem}`
-    imagem.alt = frutas.nome
+//     const imagem = document.createElement('img')
+//     imagem.src = `./img/${frutas.imagem}`
+//     imagem.alt = frutas.nome
 
-    const nome = document.createElement('h2')
-    nome.textContent = frutas.nome
+//     const nome = document.createElement('h2')
+//     nome.textContent = frutas.nome
 
-    const color = document.createElement('h3')
-    color.textContent = 'Cor: ${frutas.color}'
+//     const color = document.createElement('h3')
+//     color.textContent = 'Cor: ${frutas.color}'
 
-    const family = document.createElement('h4')
-    family.textContent = 'Família: ${frutas.family}'
+//     const family = document.createElement('h4')
+//     family.textContent = 'Família: ${frutas.family}'
 
-    card.appendChild(imagem)
-    card.appendChild(nome)
-    card.appendChild(family)
-    card.appendChild(color)
+//     card.appendChild(imagem)
+//     card.appendChild(nome)
+//     card.appendChild(family)
+//     card.appendChild(color)
 
-    return card
-}
+//     return card
+// }
 
-const mostrarCards = (frutas) => {
-    const cardContainer = document.getElementById('card-container')
-    const card = criarCards(frutas)
-    cardContainer.appendChild(card)
+// const mostrarCards = (frutas) => {
+//     const cardContainer = document.getElementById('card-container')
+//     const card = criarCards(frutas)
+//     cardContainer.appendChild(card)
 
-}
+// }
 
-frutas.forEach(mostrarCards)
+// frutas.forEach(mostrarCards)
